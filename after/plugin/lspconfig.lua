ae670069fc102428558c29d147a2b428aadde2db
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-04-01 20:27:12 +0200

    Configure LSP properly

---
+++ after/plugin/lspconfig.lua
@@ -0,0 +1,64 @@
+local lspconfig = require("lspconfig")
+
+local on_attach = function(client, bufnr)
+	client.server_capabilities.documentFormattingProvider = false
+	client.server_capabilities.documentRangeFormattingProvider = false
+
+	local opts = { buffer = bufnr, remap = false }
+
+	vim.keymap.set("n", "gd", function()
+		vim.lsp.buf.definition()
+	end, opts)
+	vim.keymap.set("n", "K", function()
+		vim.lsp.buf.hover()
+	end, opts)
+	vim.keymap.set("n", "<leader>vd", function()
+		vim.diagnostic.open_float()
+	end, opts)
+	vim.keymap.set("n", "[d", function()
+		vim.diagnostic.goto_next()
+	end, opts)
+	vim.keymap.set("n", "]d", function()
+		vim.diagnostic.goto_prev()
+	end, opts)
+	vim.keymap.set("n", "<leader>ca", function()
+		vim.lsp.buf.code_action()
+	end, opts)
+	vim.keymap.set("n", "<leader>gr", function()
+		vim.lsp.buf.references()
+	end, opts)
+	vim.keymap.set("n", "<leader>rn", function()
+		vim.lsp.buf.rename()
+	end, opts)
+end
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+lspconfig.pyright.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+lspconfig.tsserver.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+lspconfig.lua_ls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+})
+
+require("flutter-tools").setup({
+	lsp = {
+		on_attach = on_attach,
+		capabilities = capabilities,
+	},
+})
--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -2,14 +2,9 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.autopep8,
-        null_ls.builtins.diagnostics.eslint,
-        -- null_ls.builtins.diagnostics.flake8,
-        null_ls.builtins.code_actions.eslint,
-        -- null_ls.builtins.code_actions.refactoring,
-        null_ls.builtins.diagnostics.php,
-        null_ls.builtins.formatting.pint
+      null_ls.builtins.diagnostics.ruff,
+      null_ls.builtins.diagnostics.mypy,
+      null_ls.builtins.formatting.black,
+      null_ls.builtins.formatting.stylua,
     },
 })
