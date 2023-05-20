f9433ead3b85b4f57d6c49c9b1d0fb1c0be373eb
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-05-20 16:37:32 +0300

    Remove quick make executable

--- lua/attai/remap.lua
+++ lua/attai/remap.lua
@@ -52,7 +52,7 @@ vim.keymap.set("n", "<leader>fr", vim.lsp.buf.format)
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- Quickly make a file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Easy way to source a file
 vim.keymap.set("n", "<leader><leader>", function()
