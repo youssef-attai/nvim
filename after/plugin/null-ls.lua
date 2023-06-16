a3d5e9b353ea72fc5da43df19b2d27a57046b619
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-06-16 16:31:26 +0300

    Remove useless null-ls stuff

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -6,9 +6,9 @@ null_ls.setup({
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.autopep8,
         null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.diagnostics.flake8,
+        -- null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.code_actions.eslint,
-        null_ls.builtins.code_actions.refactoring,
+        -- null_ls.builtins.code_actions.refactoring,
         null_ls.builtins.diagnostics.php,
         null_ls.builtins.formatting.pint
     },
