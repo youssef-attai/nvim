1e2863ad0174b5cc3a1cc621ca637e6ba6dffd59
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-06-30 21:14:22 +0300

    Add markdown linting and formatting

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -2,9 +2,16 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
+      -- Markdown
+      null_ls.builtins.diagnostics.markdownlint,
+      null_ls.builtins.formatting.markdownlint,
+
+      -- Python
       null_ls.builtins.diagnostics.ruff,
       null_ls.builtins.diagnostics.mypy,
       null_ls.builtins.formatting.black,
+
+      -- Lua
       null_ls.builtins.formatting.stylua,
     },
 })
