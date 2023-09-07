8cf40b74ce19200741a08223dcab4fe36003f37d
Youssef Atta'i <youssefgalalnazem@gmail.com>
2023-09-07 16:50:06 +0300

    make floaterm bigger

--- lua/attai/set.lua
+++ lua/attai/set.lua
@@ -38,8 +38,8 @@ vim.cmd("set clipboard=unnamedplus")
 
 vim.g.floaterm_gitcommit = "floaterm"
 vim.g.floaterm_autoinsert = 1
-vim.g.floaterm_width = 0.8
-vim.g.floaterm_height = 0.8
+vim.g.floaterm_width = 0.99
+vim.g.floaterm_height = 0.99
 vim.g.floaterm_wintitle = 0
 vim.g.floaterm_autoclose = 1
 
